9eded2b68b49dee5e677b938964320ff867c5e62
João Pedro <142614398+JooJ104@users.noreply.github.com>
2024-05-23 15:03:36 -0300

    Inserindo Login-Cadastro

---
+++ Login-Cadastro/db/organela.sql
@@ -0,0 +1,21 @@
+create database Organelas;
+use Organelas;
+
+create table usuario (
+idUser INT PRIMARY KEY ,
+username VARCHAR (20) NOT NULL,
+password VARCHAR(8) NOT NULL
+);
+
+RENAME TABLE usuario to user;
+
+insert into user (idUser, username, password) values
+(1,'João','teste');
+
+insert into user (idUser, username, password) values
+(2,'Roberto','banana');
+
+insert into user (idUser, username, password) values
+(3,'Ryan','ovelha');
+
+select * from user;
